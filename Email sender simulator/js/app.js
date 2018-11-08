d4fc4e18f9dfa964a261d62f1881f468c2596146
Rubén <rubenzc@me.com>
2018-11-08 18:05:10 +0100

    Added check length input function

--- Email sender simulator/js/app.js
+++ Email sender simulator/js/app.js
@@ -15,13 +15,49 @@ function eventlisteners(){
     //Begin app & disable send button
     document.addEventListener('DOMContentLoaded', inicioApp)
 
+    //Form fields
+    email.addEventListener('blur', validarCampo);
+    asunto.addEventListener('blur', validarCampo);
+    mensaje.addEventListener('blur', validarCampo);
+
+
+
+
 }
 
 
 //FUNCTIONS
 
+//Init app
 function inicioApp(){
-
+    //Disable send button
     btnEnviar.disabled = true;
+}
+
+//Check fields form
+function validarCampo(){
+
+    //Check input length and not empty
+    validarLongitud(this);
+
+    //Check the field if it's an email
+    if(this.type === 'email'){
+        validarEmail(this);
+    }
 
 }
+
+//Check input length in the fields
+function validarLongitud(campo){
+
+    if (campo.value.length > 0){
+        campo.style.borderBottomColor = 'green'
+        campo.classList.remove('error');
+    } else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
+
+}
+
+//
