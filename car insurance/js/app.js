4fedda772cca78416b0a702fd3fee5839db3fac1
Rubén <rubenzc@me.com>
2018-11-20 17:10:32 +0100

    Added final result

--- car insurance/js/app.js
+++ car insurance/js/app.js
@@ -5,13 +5,51 @@ function Seguro(marca, anio, tipo){
     this.tipo = tipo;
 }
 
+Seguro.prototype.cotizarSeguro = function(info){
+     /*
+          1 = american 1.15
+          2 = asiatic 1.05
+          3 = european 1.35
+     */
+
+     let cantidad;
+     const base = 2000;
+
+     switch(this.marca){
+         case '1':
+            cantidad = base *1.15;
+            break;
+        case '2':
+            cantidad = base * 1.05;
+            break;
+        case '3':
+            cantidad = base * 1.35;
+            break;
+    }
+    //Read the yaer
+    const diferencia = new Date().getFullYear( - this.anio);
+    //Each year of difference we have to decrease 3% the value of secure
+    cantidad -= ((diferencia + 3) + cantidad) / 100;
+
+    /*
+        If secure is basic * 30%
+        if secure is full * 50%
+    */
+    if(this.tipo === 'basico') {
+        cantidad *= 1.30;
+    } else {
+        cantidad *= 1.50;
+    }
+
+    return cantidad;
+
+}
 
 //Interface constructor
 function Interfaz(){}
 
 //Message to print in the HTML
 
-// Mensaje que se imprime en el HTML
 Interfaz.prototype.mostrarMensaje = function(mensaje, tipo) {
     const div = document.createElement('div');
 
@@ -26,7 +64,44 @@ Interfaz.prototype.mostrarMensaje = function(mensaje, tipo) {
     setTimeout(function() {
          document.querySelector('.mensaje').remove();
     }, 3000);
-} 
+}
+
+//Print the final result
+Interfaz.prototype.mostrarResultado = function(seguro, total) {
+    const resultado = document.getElementById('resultado');
+    let marca;
+    switch(seguro.marca) {
+         case '1':
+              marca = 'Americano';
+              break;
+         case '2':
+              marca = 'Asiatico';
+              break;
+         case '3':
+              marca = 'Europeo';
+              break;
+    }
+    // Create a div
+    const div = document.createElement('div');
+    // Insert the info
+    div.innerHTML = `
+         <p class='header'>Tu Resumen: </p>
+         <p>Marca: ${marca} </p>
+         <p>Año: ${seguro.anio} </p>
+         <p>Tipo: ${seguro.tipo} </p>
+         <p> Total: $ ${total} </p>
+    `;
+
+    const spinner = document.querySelector('#cargando img');
+    spinner.style.display = 'block';
+    setTimeout(function() {
+         spinner.style.display = 'none';
+         resultado.appendChild(div);
+    }, 3000);
+    
+
+}
+
 
 //Event listeners
 const formulario = document.getElementById('cotizar-seguro');
@@ -49,12 +124,24 @@ formulario.addEventListener('submit', function(e){
     const interfaz = new Interfaz();
 
     //No empty fields
-    if(marcaSeleccionada === '' || anioSeleccionado === '' || tipo === ''){
-        //Interface printing an error
+    if(marcaSeleccionada === '' || anioSeleccionado === '' || tipo === '') {
+        // Interface printing an error
         interfaz.mostrarMensaje('Faltan datos, revisar el formulario y prueba de nuevo', 'error');
-    } else {
-        console.log('Todo correcto');
-    }
+   } else {
+        // Clean before results
+        const resultados = document.querySelector('#resultado div');
+        if(resultados != null) {
+             resultados.remove();
+        }
+
+        // Instance secure & show interface
+        const seguro = new Seguro(marcaSeleccionada, anioSeleccionado, tipo);
+        // Cotizar seguro
+        const cantidad = seguro.cotizarSeguro();
+        // Show result
+        interfaz.mostrarResultado(seguro, cantidad);
+        interfaz.mostrarMensaje('Cotizando...', 'exito');
+   }
 
 });
 
